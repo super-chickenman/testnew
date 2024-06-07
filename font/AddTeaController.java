d60694fc66864803e19440134bff8a0bd9979f93
super-chickenman <hanshuo2023@mail.ustc.edu.cn>
2024-06-07 14:11:04 +0800

    uf

--- font/AddTeaController.java
+++ font/AddTeaController.java
@@ -1,11 +1,19 @@
 package admin_jar.font;
 
 import admin_jar.Main.Admin;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class AddTeaController {
 
@@ -70,5 +78,6 @@ public class AddTeaController {
         new Admin().showMessage("提示","教师添加成功！", Alert.AlertType.INFORMATION,0);
         stage.close();
     }
+
 
 }
